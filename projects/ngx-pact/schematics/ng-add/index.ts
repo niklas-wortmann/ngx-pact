18a534c646c0e040ab683bcf6e7e7e94bf11e2a7
Jan-Niklas Wortmann <6104311+JWO719@users.noreply.github.com>
2020-03-30 10:08:01 +0200

    feat(karma): cleanup karma change code

--- projects/ngx-pact/schematics/ng-add/index.ts
+++ projects/ngx-pact/schematics/ng-add/index.ts
@@ -2,23 +2,16 @@ import {
   chain,
   Rule,
   SchematicContext,
-  SchematicsException,
-  Tree
+  Tree,
+  noop
 } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import * as ts from 'typescript';
 import { Schema } from './schema';
 import {
   addConfigObjectToPackageJson,
   addPactDependencies
 } from './utils/package';
-import { getProject } from './utils/project';
-import {
-  InsertChange,
-  createChangeRecorder,
-  commitChanges
-} from './utils/change';
-import { CurrencyPipe } from '@angular/common';
+import { pactifyKarmaConf } from './utils/karma';
 
 const addPactConfigToPackageJSON = ({
   pactBinaryLocation,
@@ -44,96 +37,18 @@ const updatePackageJSON = () => {
   };
 };
 
-const updateKarmaConf = (options: Schema) => {
-  return (host: Tree) => {
-    const project = getProject(host, options);
-    const path = project.root;
-    if (!project.architect) {
-      throw new SchematicsException(
-        `angular.json is wrongly configured for ${options.project}`
-      );
-    }
-    const karmaConfigPath = project.architect.test.options.karmaConfig;
-    const karmaConfigBuffer = host.read(karmaConfigPath);
-    if (karmaConfigBuffer === null) {
-      throw new SchematicsException(`Could not find (${karmaConfigPath})`);
-    }
-    const karmaConfig = ts.createSourceFile(
-      'karma.conf.js',
-      karmaConfigBuffer.toString('utf-8'),
-      ts.ScriptTarget.Latest,
-      false,
-      ts.ScriptKind.JS
-    );
-    const moduleStatement = karmaConfig.statements.find(
-      node => node.kind === ts.SyntaxKind.ExpressionStatement
-    );
-    if (moduleStatement) {
-      const [
-        changes
-      ] = (moduleStatement as any).expression.right.body.statements.map(
-        (node: any) => {
-          const [iteration] = node.expression.arguments.map((prop: any) => {
-            const confKeys = prop.properties.reduce((acc: any, curr: any) => {
-              if (['frameworks', 'plugins'].includes(curr.name.escapedText)) {
-                return {
-                  ...acc,
-                  ...{ [curr.name.escapedText]: curr.initializer.elements.end }
-                };
-              }
-              return acc;
-            }, {});
-            return { ...confKeys, ...{ end: prop.properties.end } };
-          });
-          return iteration;
-        }
-      );
-      const pactConfiguration = `,
-    pact: [
-      {
-        cors: true,
-        port: 1234,
-        log: path.resolve(process.cwd(), 'logs', 'mockserver-integration.log'),
-        dir: path.resolve(process.cwd(), '../../pacts')
-      }
-    ]`;
-      const pactFrameworkConfiguration = `, 'pact'`;
-      const pluginConfiguration = `,
-      require('@pact-foundation/karma-pact')`;
-
-      const pactConfigChange = new InsertChange(
-        karmaConfigPath,
-        changes.end,
-        pactConfiguration
-      );
-      const frameWorkChange = new InsertChange(
-        karmaConfigPath,
-        changes.frameworks,
-        pactFrameworkConfiguration
-      );
-      const pluginChange = new InsertChange(
-        karmaConfigPath,
-        changes.plugins,
-        pluginConfiguration
-      );
-      commitChanges(host, karmaConfigPath, [
-        pactConfigChange,
-        frameWorkChange,
-        pluginChange
-      ]);
-    }
-    return host;
-  };
+const updateKarmaConfig = (options: Schema) => {
+  return (host: Tree) => pactifyKarmaConf(host, options);
 };
 
 export function ngAdd(options: Schema): Rule {
   return (tree: Tree, context: SchematicContext) => {
     return chain([
-      // options.pactBinaryLocation || options.pactDoNotTrack
-      //   ? addPactConfigToPackageJSON(options)
-      //   : noop(),
-      // options.skipInstall ? noop() : updatePackageJSON(),
-      updateKarmaConf(options)
+      options.pactBinaryLocation || options.pactDoNotTrack
+        ? addPactConfigToPackageJSON(options)
+        : noop(),
+      options.skipInstall ? noop() : updatePackageJSON(),
+      updateKarmaConfig(options)
     ])(tree, context);
   };
 }
---
+++ projects/ngx-pact/schematics/ng-add/utils/karma.ts
@@ -0,0 +1,97 @@
+import { SchematicsException, Tree } from '@angular-devkit/schematics';
+import * as ts from 'typescript';
+import { Schema } from '../schema';
+import { commitChanges, InsertChange } from './change';
+import { getProject } from './project';
+
+const pactConfiguration = `,
+pact: [
+  {
+    cors: true,
+    port: 1234,
+    log: path.resolve(process.cwd(), 'logs', 'mockserver-integration.log'),
+    dir: path.resolve(process.cwd(), '../../pacts')
+  }
+]`;
+const pactFrameworkConfiguration = `, 'pact'`;
+const pluginConfiguration = `,
+  require('@pact-foundation/karma-pact')`;
+
+export const pactifyKarmaConf = (host: Tree, options: Schema) => {
+  const project = getProject(host, options);
+  if (!project.architect) {
+    throw new SchematicsException(
+      `angular.json is wrongly configured for ${options.project}`
+    );
+  }
+  const karmaConfigPath = project.architect.test.options.karmaConfig;
+  const karmaConfigBuffer = host.read(karmaConfigPath);
+  if (karmaConfigBuffer === null) {
+    throw new SchematicsException(`Could not find (${karmaConfigPath})`);
+  }
+  const karmaConfig = ts.createSourceFile(
+    'karma.conf.js',
+    karmaConfigBuffer.toString('utf-8'),
+    ts.ScriptTarget.Latest,
+    false,
+    ts.ScriptKind.JS
+  );
+  const changes = getChangePositions(karmaConfig);
+
+  const pactConfigChange = new InsertChange(
+    karmaConfigPath,
+    changes.end,
+    pactConfiguration
+  );
+  const frameWorkChange = new InsertChange(
+    karmaConfigPath,
+    changes.frameworks,
+    pactFrameworkConfiguration
+  );
+  const pluginChange = new InsertChange(
+    karmaConfigPath,
+    changes.plugins,
+    pluginConfiguration
+  );
+  commitChanges(host, karmaConfigPath, [
+    pactConfigChange,
+    frameWorkChange,
+    pluginChange
+  ]);
+  return host;
+};
+
+interface KarmaChangePositions {
+  end: number;
+  frameworks: number;
+  plugins: number;
+}
+
+const getChangePositions = (source: ts.SourceFile): KarmaChangePositions => {
+  const moduleStatement = source.statements.find(
+    node => node.kind === ts.SyntaxKind.ExpressionStatement
+  );
+  if (!moduleStatement) {
+    throw new SchematicsException(`karma.config.js in an unexpected shape`);
+  }
+  const [
+    changes
+  ] = (moduleStatement as any).expression.right.body.statements.map(
+    (node: any) => {
+      const [iteration] = node.expression.arguments.map((prop: any) => {
+        const confKeys = prop.properties.reduce((acc: any, curr: any) => {
+          if (['frameworks', 'plugins'].includes(curr.name.escapedText)) {
+            return {
+              ...acc,
+              ...{ [curr.name.escapedText]: curr.initializer.elements.end }
+            };
+          }
+          return acc;
+        }, {});
+        return { ...confKeys, ...{ end: prop.properties.end } };
+      });
+      return iteration;
+    }
+  );
+  return changes;
+};
